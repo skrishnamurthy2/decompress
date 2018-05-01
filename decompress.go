742017dd80be8321b188db76ffa63d56b8a4bf79
Sridhar Krishnamurthy <sridhark@zillowgroup.com>
2018-05-01 09:31:14 -0700

    Decompress string compressed

---
+++ decompress.go
@@ -0,0 +1,97 @@
+package decompress
+
+import (
+  "strconv"
+)
+
+func sliceRepeatCopy(src [] byte, repeat int) []byte {
+
+  destination := make([]byte, 0)
+
+  for i := 0; i < repeat; i++ {
+
+	for _, sliceValue := range src {
+	  destination = append(destination, sliceValue)
+	}
+
+  }
+  return destination
+}
+
+func slideAppend(dest [] byte, src [] byte) []byte {
+
+  for _, uncompByte := range src {
+	dest = append(dest, uncompByte)
+  }
+  return dest
+}
+
+func decompress(compressed []byte, finishEarly bool) ([]byte, int) {
+
+  var (
+	dataCompressed = make([]byte, 0)
+	timesCompressed = make([]byte, 0)
+	insideCompression = false
+	index = 0
+	uncompressedResult = make([]byte, 0)
+  )
+
+  for index < len(compressed) {
+
+    if compressed[index] == '[' {
+
+	  insideCompression = true
+	  index++
+
+	} else if(compressed[index] == ']') {
+
+	  insideCompression = false
+	  index++
+
+	  repeat, _ := strconv.Atoi(string(timesCompressed))
+
+	  uncompressedResult = slideAppend(uncompressedResult, sliceRepeatCopy(dataCompressed, repeat))
+
+	  if finishEarly {
+	    return uncompressedResult, index
+	  }
+
+	  timesCompressed = make([]byte, 0)
+	  dataCompressed = make([]byte, 0)
+
+	} else {
+
+	  if compressed[index] >= '0' && compressed[index] <= '9' {
+
+		if insideCompression {
+
+		  result, indexProcessed := decompress(compressed[index:], true)
+		  index += indexProcessed - 1
+
+		  dataCompressed = slideAppend(dataCompressed, result)
+
+		} else {
+
+		  timesCompressed = append(timesCompressed, compressed[index])
+		}
+
+	  } else {
+
+	    if(insideCompression) {
+
+		  dataCompressed = append(dataCompressed, compressed[index])
+		} else {
+
+		  uncompressedResult = append(uncompressedResult, compressed[index])
+
+		}
+	  }
+
+	  index++;
+	}
+
+  }
+
+  return uncompressedResult, index
+
+}
---
+++ decompress_test.go
@@ -0,0 +1,86 @@
+package decompress
+
+import (
+  "testing"
+)
+
+func TestDecompressSimpleString(t *testing.T) {
+  decompressed, _ := decompress([]byte("abc"), false)
+
+  if string(decompressed) != "abc" {
+    t.Fail()
+  }
+
+}
+
+func TestDecompressSingleCompression(t *testing.T) {
+  decompressed, _ := decompress([]byte("3[abc]"), false)
+
+  if string(decompressed) != "abcabcabc" {
+	t.Fail()
+  }
+
+}
+
+func TestDecompressSingleCompressionPlusSimpleString(t *testing.T) {
+  decompressed, _ := decompress([]byte("3[abc]xyz"), false)
+
+  if string(decompressed) != "abcabcabcxyz" {
+	t.Fail()
+  }
+
+}
+
+func TestDecompresshMultipleLinearCompression(t *testing.T) {
+  decompressed, _ := decompress([]byte("3[abc]2[efg]"), false)
+
+  if string(decompressed) != "abcabcabcefgefg" {
+	t.Fail()
+  }
+
+}
+
+func TestDecompressMultipleLinearCompressionWithSimpleStringBetween(t *testing.T) {
+  decompressed, _ := decompress([]byte("3[abc]gosh2[efg]"), false)
+
+  if string(decompressed) != "abcabcabcgoshefgefg" {
+	t.Fail()
+  }
+
+}
+
+func TestDecompressSimpleNested(t *testing.T) {
+  decompressed, _ := decompress([]byte("3[2[a]]"), false)
+
+  if string(decompressed) != "aaaaaa" {
+	t.Fail()
+  }
+
+}
+
+func TestDecompressMultipleNested(t *testing.T) {
+  decompressed, _ := decompress([]byte("3[2[3[a]]]"), false)
+
+  if string(decompressed) != "aaaaaaaaaaaaaaaaaa" {
+	t.Fail()
+  }
+
+}
+
+func TestDecompressSimpleNestedAndLinearString(t *testing.T) {
+  decompressed, _ := decompress([]byte("3[2[a]c]"), false)
+
+  if string(decompressed) != "aacaacaac" {
+	t.Fail()
+  }
+
+}
+
+func TestDecompressSimpleNestedAndMultipleLinear(t *testing.T) {
+  decompressed, _ := decompress([]byte("3[2[a]2[c]]"), false)
+
+  if string(decompressed) != "aaccaaccaacc" {
+	t.Fail()
+  }
+
+}
